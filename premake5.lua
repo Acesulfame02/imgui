e141ec635291cd615b08757a6588f0df8a0791cf
Aaron Masembe <aaronmasembemujabikalibala@gmail.com>
2024-04-12 07:28:02 +0200

    imgui added

--- premake5.lua
+++ premake5.lua
@@ -12,11 +12,11 @@ project "IMGUI"
     "imgui.cpp",
     "imgui_draw.cpp",
     "imgui_internal.h",
-    "imgui_wingets.cpp",
+    "imgui_widgets.cpp",
     "imstb_rectpack.h",
-    "imstb_textedit.h",,
+    "imstb_textedit.h",
     "imstb_truetype.h",
-    "imgui_demo.cpp",
+    "imgui_demo.cpp"
   }
 
   filter "system:windows"
